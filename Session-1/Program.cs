d2318a7475025b2c7066ff239ddf86170332f508
200~mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2024-12-30 11:29:29 +0200

    Struct

--- Session-1/Program.cs
+++ Session-1/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Struct : Value Type :Stack
+
+            Point P01;
+            //Declare For Object From Type 'Point'
+            //P01:Object
+            //Allocate UnInitialized 8 Bytes At The Stack
+
+            P01.X = 12;
+            P01.Y = 13;
+            Console.WriteLine(P01.X);
+            Console.WriteLine(P01.Y);
+
+            // when you use new so you use the constructor which set all Variables to default value(0)
+            P01 = new Point(1, 2);
+            Console.WriteLine(P01.Y);
+
+
+            //Console.WriteLine(P01); // Session_1.Point
+
+            //P01.PrintPoint();
+
+
+            //Console.WriteLine(P01.ToString()); //Session_1.Point
+
+            // output change based on the override on tostring
+            Console.WriteLine(P01.ToString()); //(1,2)
+
+            // output change based on the override on tostring
+            Console.WriteLine(P01); //(1,2)
+
+
+
+
+
         }
     }
 }
