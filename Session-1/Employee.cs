77db032ed8fe255cf68b47fe974e246b9d20e763
200~mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-01-01 09:53:31 +0200

    no thing

---
+++ Session-1/Employee.cs
@@ -0,0 +1,79 @@
+﻿namespace Session_1
+{
+    internal struct Employee
+    {
+        private int id;
+        private string name;
+        private double salary;
+
+        // Apply Encapsulation User Setter Getter Method
+
+        #region Encapsulation User Setter Getter Method
+        //public void SetId(int id)
+        //{
+        //    this.id = id;
+        //}
+        //public int GetId()
+        //{
+        //    return id;
+        //}
+        //public void SetName(string name)
+        //{
+        //    this.name = name;
+        //}
+        //public string GetName()
+        //{
+        //    return name;
+        //}
+        //public void SetSalary(double Salary)
+        //{
+        //    this.Salary = Salary;
+        //}
+        //public double GetSalary()
+        //{
+        //    return Salary;
+        //}
+
+        //public Employee(int id, string name, double salary)
+        //{
+        //    SetId(id);
+        //    SetName(name);
+        //    SetSalary(Salary);
+        //} 
+        #endregion
+
+
+        // Apply Encapsulation User Properties
+
+
+        #region Encapsulation Using Properties
+
+        //1. Full Property
+        public int Id
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public string Name
+        {
+            set { 
+                if(value.Length>=0 && value.Length<=20)
+                    name = value;
+            }
+            get { return name; }
+        }
+        public double Salary
+        {
+            set { 
+                if(value>0)
+                salary = value; }
+            get { return salary; }
+        }
+
+
+        // Automatic Property
+        public string Address { set; get; }
+
+        #endregion
+    }
+}
---
+++ Session-1/PhoneBook.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Session_1
+{
+    internal struct PhoneBook
+    {
+        string[] names;
+        long[] numbers;
+
+        // constructor take size to create struct Arrays
+        public PhoneBook(int size)
+        {
+            names = new string[size];
+            numbers = new long[size];
+        }
+
+        // this give us ability to treate Struct like array
+        public long this[string name]
+        {
+            set {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] == name)
+                        numbers[i] = value;
+                }
+            }
+            get {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] == name)
+                        return numbers[i];
+                }
+                return -1;
+            }
+        }
+        public string this[long number]
+        {
+            set {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (numbers[i] == number)
+                        names[i] = value;
+                }
+            }
+            get {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (numbers[i] == number)
+                        return names[i];
+                }
+                return "";
+            }
+        }
+
+
+        // create functions to manage the struct
+        public void AddPerson(string name,long number,int index)
+        {
+            names[index] = name;
+            numbers[index] = number;
+        }
+        public long GetNumber(string name)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == name)
+                    return numbers[i];
+            }
+            return -1;
+        }
+        public void updateNumber(string name,long number)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == name)
+                     numbers[i]=number;
+            }
+        }
+    }
+}
